326a0a8cfe73a8fed01da1d12f231aed5a45a1d7
Ankit Pakhale <akp3067@gmail.com>
2023-01-19 15:23:49 +0530

    Added: routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import Alert from "react-bootstrap/Alert";
 import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path={"/"} exact component={ProductListing} />
+          <Route
+            path={"/product/:productId"}
+            exact
+            component={ProductDetails}
+          />
+          <Route>404 Not Found!!!</Route>
+
+          {/* till 23:51 */}
+        </Routes>
+      </Router>
     </div>
   );
 }
